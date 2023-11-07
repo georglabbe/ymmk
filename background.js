a64cea0ba6da48ddd540c34efb4aad9363405724
Georg Labbé <georglabbemail@gmail.com>
2023-11-07 13:21:43 +0100

    make it work

--- background.js
+++ background.js
@@ -1,41 +1,95 @@
+// set the current youtube music tab by clicking the extension icon
 chrome.action.onClicked.addListener(function (tab) {
-    chrome.storage.local.set({ key: tab.id }).then(() => {
-        console.log("now controlling tab with id", tab.id);
-    });
+    storeTabId(tab.id)
 });
 
 
-chrome.commands.onCommand.addListener((command) => {
-    console.log("media key pressed");
-    chrome.storage.local.get(["key"]).then((result) => {
-        console.log("trying to control tab", result.key);
-        switch (command) {
-            case "play-pause-media":
-                console.log("PlayPause media key pressed.");
-                chrome.scripting.executeScript({
-                    target: { tabId: result.key },
-                    function: simulateSpaceKeyPress
-                });
-                break;
-            case "next-media":
-                console.log("Next Track media key pressed.");
-                chrome.scripting.executeScript({
-                    target: { tabId: result.key },
-                    function: simulateRightArrowKeyPress
-                });
-                break;
-            case "previous-media":
-                console.log("Previous Track media key pressed.");
-                chrome.scripting.executeScript({
-                    target: { tabId: result.key },
-                    function: simulateLeftArrowKeyPress
-                });
-                break;
+// handle media key press
+chrome.commands.onCommand.addListener(async (command) => {
+    let tabId = await getStoredTabId();
+
+    // verify the existence of stored tab id
+    if (tabId != null) {
+        let tabExists = await exists(tabId)
+        if (!tabExists) {
+            tabId = null
         }
-    });
+    }
+
+    // if theres no verified tab stored, search for it
+    if (tabId == null) {
+        tabId = await findAndStoreCurrentYoutubeMusicTab();
+    }
+
+    // if nothing can be found, do nothing
+    if (tabId == null) {
+        return;
+    }
+
+    // dispatch the according key on the tab
+    switch (command) {
+        case "play-pause-media":
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                function: simulateSpaceKeyPress
+            });
+            break;
+        case "next-media":
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                function: simulateRightArrowKeyPress
+            });
+            break;
+        case "previous-media":
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                function: simulateLeftArrowKeyPress
+            });
+            break;
+    }
 });
 
+chrome.tabs.onRemoved.addListener(async(tabId) => {
+    let storedTabId = await getStoredTabId();
+    console.log("delted tab", tabId);
+    if (tabId == storedTabId) {
+        chrome.storage.local.clear();
+    }
+});
 
+// find and store the first valid music.youtube tab
+function findAndStoreCurrentYoutubeMusicTab() {
+    return new Promise((resolve) => {
+        chrome.tabs.query({}, function (tabs) {
+            const tab = tabs.find((tab) => tab.url.startsWith("https://music.youtube.com/"))
+            if (tab !== undefined) {
+                storeTabId(tab.id)
+                resolve(tab.id)
+            }
+        });
+    })
+}
+
+function getStoredTabId() {
+    return new Promise((resolve) => {
+        chrome.storage.local.get(["tabId"]).then((result) => {
+            resolve(result.tabId);
+        });
+    })
+}
+
+function exists(tabId) {
+    return new Promise((resolve) => {
+        chrome.tabs.query({}, function (tabs) {
+            let result = tabs.some((tab) => tab.id == tabId);
+            resolve(result);
+        });
+    })
+}
+
+function storeTabId(tabId) {
+    chrome.storage.local.set({ tabId: tabId });
+}
 
 function simulateSpaceKeyPress() {
     var spaceKeyEvent = new KeyboardEvent("keydown", { key: "SPACE" });
